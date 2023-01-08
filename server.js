8ddb611094fc3dea64970672ad0ddbfa70766653
Przemek-R <przemyslaw.swiercz10@gmail.com>
2023-01-08 13:55:14 +0100

    Register - bd

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 'use strict';
 
 const express = require('express');
+const routes = require('./routes/index');
 
 const PORT = 3001;
 const HOST = '0.0.0.0';
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000',
+    credentials:true,
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
+
+app.use(express.json())
+app.use('/', routes);
 
 app.listen(PORT, HOST, () => {
     console.log(`Running on http://${HOST}:${PORT}`);
